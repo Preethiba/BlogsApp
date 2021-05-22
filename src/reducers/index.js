a1568e1bf6290cec71ebfef07596b547f70184d9
Preethiba <ibpreethiba99@gmail.com>
2021-05-22 18:31:52 +0530

    2 files modified

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import getPosts from './reducer_getPosts';
 
 const rootReducer = combineReducers({
+  form: formReducer,
   posts: getPosts
 });
 
