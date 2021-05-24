a935dfa62a76305a529268c8755e59e72e7788b3
Preethiba <ibpreethiba99@gmail.com>
2021-05-24 19:22:38 +0530

    1 file modified

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostShow extends React.Component {
+  componentDidMount(){
+    this.props.fetchPost(this.props.match.params.id);
+  }
   render() {
     return (
       <div>
@@ -10,4 +15,10 @@ class PostShow extends React.Component {
   }
 }
 
-export default PostShow;
+function mapStateToProps({ posts }){
+  return{
+    post: posts
+  }
+}
+
+export default connect(mapStateToProps,{ fetchPost }(PostShow);
