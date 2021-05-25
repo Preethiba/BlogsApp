04b59541d997958547f39bb9d00aa8e35c55cae4
Preethiba <ibpreethiba99@gmail.com>
2021-05-25 11:17:08 +0530

    1 file modified

--- src/reducers/reducer_getPosts.js
+++ src/reducers/reducer_getPosts.js
@@ -11,6 +11,8 @@ export default function(state = {}, action) {
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return _.omit(state, action.payload); //to delete an item from an object through lodash
     default:
       return state;
   }
